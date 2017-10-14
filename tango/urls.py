4cba433943651d50f0a3d52be3cc71ea40c5232e
lbias <306508354@qq.com>
2017-10-14 21:05:26 +0800

    url rango#index

--- tango/urls.py
+++ tango/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    # above maps any URLs starting
+    # with rango/ to be handled by
+    # the rango application
     url(r'^admin/', admin.site.urls),
 ]

151c2669fa191d18cc3d6abf43925f6460aca71c
lbias <306508354@qq.com>
2017-10-15 11:47:34 +0800

    add attributes views, likes to model Category

--- populate_rango.py
+++ populate_rango.py
@@ -8,30 +8,30 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-		{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/"},
-		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/"},
-	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/"},  ]
+		{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 32},
+		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
+	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 8},  ]
 
     django_pages = [
 		{"title":"Official Django Tutorial",
-	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
 		{"title":"Django Rocks",
-	        "url":"http://www.djangorocks.com/" },
+	        "url":"http://www.djangorocks.com/" , "views": 16},
 		{ "title":"How to Tango with Django",
-	        "url":"http://www.tangowithdjango.com/"} ]
+	        "url":"http://www.tangowithdjango.com/", "views": 8 } ]
 
     other_pages = [
-		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/"},
-		{ "title":"Flask", "url":"http://flask.pocoo.org"} ]
+		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
+		{ "title":"Flask", "url":"http://flask.pocoo.org", "views": 16} ]
 
-    cats = {"Python": {"pages": python_pages},
-			"Django": {"pages": django_pages},
-        	"Other Frameworks": {"pages": other_pages},
-            "Pascal": {"pages": []},
-            "Perl": {"pages": []},
-            "Php": {"pages": []},
-            "Prolog": {"pages": []},
-            "Programming": {"pages": []},
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+			"Django": {"pages": django_pages, "views": 64, "likes": 32},
+        	"Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
+            "Pascal": {"pages": [], "views": 32, "likes": 16},
+            "Perl": {"pages": [], "views": 32, "likes": 16},
+            "Php": {"pages": [], "views": 32, "likes": 16},
+            "Prolog": {"pages": [], "views": 32, "likes": 16},
+            "Programming": {"pages": [], "views": 32, "likes": 16},
 
             }
 
@@ -45,9 +45,11 @@ def populate():
 
     # Using the .items returns the key and the value. In this case the key is "Python", "Django" or "Other Frameworks" and the value (cat_data) is the corresponding dictionary in cats.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        # c = add_cat(cat)
+        # Updated the population script to pass through the specific values for views and likes
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
@@ -62,8 +64,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 

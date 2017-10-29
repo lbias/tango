d3d262f5cc174a57ab91283912c9bd0d71d4163d
lbias <306508354@qq.com>
2017-10-29 17:55:26 +0800

    model UserProfile fields website, picture

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -17,6 +19,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
@@ -25,3 +28,15 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    # Override the __unicode__() method to return out something meaningful!
+    def __str__(self):
+        return self.user.username
